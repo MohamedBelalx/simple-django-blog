5b38a3420046d583e7d08dff43ed9b1ad4f5c55a
MohamedBelalx <hero.armna@gmail.com>
2020-05-28 18:13:00 +0200

    config setting for templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('this is simple test')
+    return render(request,'index.htm')
