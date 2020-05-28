52beb8cf7533a3901daaaf0b618cf3efcbea91ae
MohamedBelalx <hero.armna@gmail.com>
2020-05-28 19:12:44 +0200

    create Article Model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    title = models.CharField()
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.models.DateTimeField(auto_now_add=True)
