c7c1342c0bcb202eacfa05db1e5f2ad05c4038d7
MohamedBelalx <hero.armna@gmail.com>
2020-05-29 13:36:36 +0200

    fix str naming

--- articles/models.py
+++ articles/models.py
@@ -6,3 +6,10 @@ class Articles(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
