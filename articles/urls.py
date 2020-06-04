41803a710518e4a4ff5107fb78089ec0528a719e
MohamedBelalx <hero.armna@gmail.com>
2020-06-04 19:31:37 +0200

    add details page

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+app_name = 'articles'
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('<int:pk>',views.with_id,name='details')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
 from .models import Articles
+
 # Create your views here.
 def index(request):
     articles = Articles.objects.all()
     return render(request,'articles/test.htm',{'articles':articles})
+
+
+def with_id(request,pk):
+    test = get_object_or_404(Articles,pk=pk)
+    return render(request,'articles/test2.htm',{'article':test})
